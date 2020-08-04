978452c9d1107d3a14c69b8bb5b9f42c5fdbcd7e
Laryssa Yumi <laryyumi@gmail.com>
2020-08-04 12:53:07 -0300

    Criação do projeto.

---
+++ src/main/java/br/com/livrosecommerce/LivrosEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.livrosecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/livrosecommerce/LivrosEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.livrosecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
